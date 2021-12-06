59ab733216f83a2e7833daa41747c0c321068140
Aurilano <aurilanosousa@hotmail.com>
2021-12-06 17:55:35 -0300

    Exercicio 1

---
+++ exerc01.php
@@ -0,0 +1,75 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<title>Exercicio 01 - Questão 06</title>
+		<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+	</head>
+
+	<body>
+		<div class="container">
+			<h3>Exercício 1, Questão 6, Calcular a média de 10 números. </h3>
+			<form action="calc.php" method="POST">
+				<div class="row">
+					  <div class="mb-3 col-md-1">
+					    <label  class="form-label"></label>
+					    <input type="number" class="form-control"  name="num1">
+					  </div>	
+
+					  <div class="mb-3 col-md-1">
+					    <label  class="form-label"></label>
+					    <input type="number" class="form-control"  name="num2">
+					  </div>	
+
+					  <div class="mb-3 col-md-1">
+					    <label  class="form-label"></label>
+					    <input type="number" class="form-control"  name="num3">
+					  </div>
+
+					 <div class="mb-3 col-md-1">
+					    <label  class="form-label"></label>
+					    <input type="number" class="form-control"  name="num4">
+					  </div>	
+
+					  <div class="mb-3 col-md-1">
+					    <label  class="form-label"></label>
+					    <input type="number" class="form-control"  name="num5">
+					  </div>
+
+					  <div class="mb-3 col-md-1">
+					    <label  class="form-label"></label>
+					    <input type="number" class="form-control"  name="num6">
+					  </div>	
+
+					  <div class="mb-3 col-md-1">
+					    <label  class="form-label"></label>
+					    <input type="number" class="form-control"  name="num7">
+					  </div>	
+
+					  <div class="mb-3 col-md-1">
+					    <label  class="form-label"></label>
+					    <input type="number" class="form-control"  name="num8">
+					  </div>	
+
+					  <div class="mb-3 col-md-1">
+					    <label  class="form-label"></label>
+					    <input type="number" class="form-control"  name="num9">
+					  </div>	
+
+					  <div class="mb-3 col-md-1">
+					    <label  class="form-label"></label>
+					    <input type="number" class="form-control"  name="num10">
+					  </div>	
+
+					 <div class="mb-1 col-md-1">
+					 	<label  class="form-label"></label>
+				  		<button type="submit" class="btn btn-primary">Calcular</button>
+				  	</div>
+				  	<h2><hr></h2>
+			  	</div>
+			</form>	
+		</div>
+		
+	</body>
+</html>
